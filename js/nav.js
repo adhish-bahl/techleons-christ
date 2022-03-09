5f32d51fd1c906ba9ea57a45ff25de1549e30e9e
Adhish Bahl <adhishbahl0@gmail.com>
2022-03-09 17:27:54 +0530

    adding mobile nav

---
+++ js/nav.js
@@ -0,0 +1,46 @@
+const handle = () => {
+    const burger = document.querySelector('.burger');
+    const sideNav = document.querySelector('.centerBox');
+    const line = document.querySelector('.line');
+
+    const teamsClick = document.querySelector('#teams-click');
+
+    teamsClick.addEventListener('click', () => {
+        sideNav.classList.toggle('slide');
+        burger.classList.toggle('active1');
+        line.classList.toggle('vanish');
+    });
+    
+    burger.addEventListener( 'click', () => {
+        sideNav.classList.toggle('slide');
+        burger.classList.toggle('active');
+        line.classList.toggle('vanish');
+    });
+}
+handle();
+// TeamSelectionHandling
+// const mario = document.querySelector('.card:nth-child(1)');
+// const contra = document.querySelector('.card:nth-child(2)');
+// const pacman = document.querySelector('.card:nth-child(3)');
+// const roadrash = document.querySelector('.card:nth-child(4)');
+// const sonic = document.querySelector('.card:nth-child(5)');
+// const tekken = document.querySelector('.card:nth-child(6)');
+
+// mario.addEventListener('click', () => {
+//     window.location.href = './teams/mario/';
+// });
+// contra.addEventListener('click', () => {
+//     window.location.href = './teams/contra/index.html';
+// });
+// pacman.addEventListener('click', () => {
+//     window.location.href = './teams/pacman/';
+// });
+// roadrash.addEventListener('click', () => {
+//     window.location.href = './teams/roadrash/';
+// });
+// sonic.addEventListener('click', () => {
+//     window.location.href = './teams/sonic/';
+// });
+// tekken.addEventListener('click', () => {
+//     window.location.href = './teams/tekken/';
+// });
